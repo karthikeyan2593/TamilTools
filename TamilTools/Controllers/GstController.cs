6fc69236ff084cb0214d5fdec7be36b4ee12a57d
Karthikeyan N <Karthikn@KarthiKn.onmicrosoft.com>
2026-05-14 21:19:34 +0530

    home

---
+++ TamilTools/Controllers/GstController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TamilTools.Controllers
+{
+    public class GstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
